71c12a6fd1c266092a61e5325acc5ed87ad0a30a
Willian Almeida <will7seven@gmail.com>
2020-04-07 11:42:05 -0300

    adicionada rotina de inicialização

--- Code/main.asm
+++ Code/main.asm
@@ -3,6 +3,104 @@
 	__config _CONFIG1, 0x2FF4
 	__config _CONFIG2, 0x3FFF
 	
-	org 0x00
+	cblock 0x20
+		led_cnt
+		aux
+	endc
+
+
+	org 0x00		;vetor de reset
+	goto Setup
+
+	org 0x04		;vetor de interrupção
+	retfie
+
+Setup:
+	bsf STATUS, RP0 ; bank1
+	movlw b'11110000'
+	movwf TRISA 	; ra0-ra3 como saida 
+	bsf STATUS, RP1 ; bank3
+	clrf ANSEL		; AN0-AN7 como IO digital
+	bcf STATUS, RP1
+	bcf STATUS, RP0 ; bank0 
+
+Loop:
+	call RotinaInicializacao
+	goto Loop
+
+
+RotinaInicializacao
+	movlw b'00001111'	; mascara or
+	iorwf PORTA, F		; aplica mascara
+	call Delay_1s
+	clrf led_cnt		;limpa registrador
+LedLoop:	
+	movlw .0
+	subwf led_cnt, W    ;compara led_cnt com 0
+	btfsc STATUS, Z
+	goto Led_0
+	
+	movlw .1
+	subwf led_cnt, W    ;compara led_cnt com 1
+	btfsc STATUS, Z
+	goto Led_1
+
+	movlw .2
+	subwf led_cnt, W    ;compara led_cnt com 2
+	btfsc STATUS, Z
+	goto Led_2
+
+	movlw .3
+	subwf led_cnt, W    ;compara led_cnt com 3
+	btfsc STATUS, Z
+	goto Led_3
+
+	movlw .4
+	subwf led_cnt, W    ;compara led_cnt com 4
+	btfss STATUS, Z
+	goto LedLoop
+	clrf PORTA
+	return
+
+Led_0:
+	movlw 0x01
+	movwf PORTA
+	call Delay_200ms
+	incf led_cnt, F
+	goto LedLoop
+
+Led_1:
+	movlw 0x02
+	movwf PORTA
+	call Delay_200ms
+	incf led_cnt, F
+	goto LedLoop
+
+Led_2:
+	movlw 0x04
+	movwf PORTA
+	call Delay_200ms
+	incf led_cnt, F
+	goto LedLoop
+
+Led_3:
+	movlw 0x08
+	movwf PORTA
+	call Delay_200ms
+	incf led_cnt, F
+	goto LedLoop
+	
+	
+
+	
+	
+
+Delay_1s
 	nop
+	return
+
+Delay_200ms
+	nop
+	return
+	
 	end
