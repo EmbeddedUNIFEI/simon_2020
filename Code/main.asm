05dc0ef7d060d8d29035d00e14a6d6b60ad1866c
Willian Almeida <will7seven@gmail.com>
2020-05-12 12:02:38 -0300

    adicionado sorteio de movimento e exibe sequencia de movimentos

--- Code/main.asm
+++ Code/main.asm
@@ -3,11 +3,25 @@
 	__config _CONFIG1, 0x2FF4
 	__config _CONFIG2, 0x3FFF
 	
+	#define LED_RED B'00000001'; bit 0
+	#define LED_YELLOW 0X02; bit 1
+	#define LED_GREEN 0x04 ; bit2
+	#define LED_BLUE 0x08; bit3
+	#define MOVE_VETOR_SIZE .32
+
 	cblock 0x20
 		led_cnt
 		aux
+		level ;level = 0(facil), 1(dificil)
+		valor_sorteado
 	endc
 
+	cblock 0x40
+		move_vetor: MOVE_VETOR_SIZE
+		move_index
+		move
+		exibe_index
+	endc
 
 	org 0x00		;vetor de reset
 	goto Setup
@@ -19,13 +33,40 @@ Setup:
 	bsf STATUS, RP0 ; bank1
 	movlw b'11110000'
 	movwf TRISA 	; ra0-ra3 como saida 
+	bsf   TRISB, TRISB4 ; botão de start (entrada)
+	bsf   TRISB, TRISB5 ; seleção de nível (entrda)
 	bsf STATUS, RP1 ; bank3
 	clrf ANSEL		; AN0-AN7 como IO digital
+	clrf ANSELH		; AN8-AN13 como IO digital
 	bcf STATUS, RP1
 	bcf STATUS, RP0 ; bank0 
-
+	clrf move_index
 Loop:
 	;call RotinaInicializacao
+
+	call SorteiaMovimento
+	movwf move
+	call ArmazenaMovimento
+	
+	call ExibeSequencia
+	goto Loop
+
+;	btfss PORTB, RB4 ;testa botão start
+	;goto Loop
+
+	btfss PORTB, RB5 ; testa seleção de dificuldade
+	goto SetLevelEasy
+	bsf level, 0 	; level = 1 (dificil)
+	goto ExitTestLevel
+SetLevelEasy
+	bcf level, 0  ; level = 0 (facil)
+
+ExitTestLevel
+
+	;SorteioMovimento
+	;ArmezenaMovimento
+	;ExibeMovimento
+	;ExibeSequencia	
 
 	;Delay 1cy
 	nop
@@ -47,6 +88,93 @@ Loop:
 	;1+1+(1+2)*(x-1) + 2
 	;3*(x-1) + 4
 	;3*x + 1
+
+
+;----Função SorteiaMovimento---------
+SorteiaMovimento:
+	addlw .8
+	movwf valor_sorteado
+	movlw B'00000011' ;mascara
+	andwf valor_sorteado, F ;filtro
+	
+TesteValor0
+	movlw .0
+	subwf valor_sorteado, W
+	btfss STATUS, Z
+	goto TesteValor1
+	retlw LED_RED
+
+TesteValor1
+	movlw .1
+	subwf valor_sorteado, W
+	btfss STATUS, Z
+	goto TesteValor2
+	retlw LED_YELLOW
+
+TesteValor2
+	movlw .2
+	subwf valor_sorteado, W
+	btfss STATUS, Z
+	goto TesteValor3
+	retlw LED_GREEN
+
+TesteValor3
+	retlw LED_BLUE
+
+;---------------------------------
+
+;-----Funçao ArmazenaMovimento----
+;Argumentos: move
+ArmazenaMovimento:
+	bcf STATUS, IRP ;banco 0 ou 1
+	movlw move_vetor
+	movwf FSR
+	movf move_index, W
+	addwf FSR, F
+	movf move, W
+	movwf INDF
+	incf move_index, F
+	return
+;---------------------------------
+
+
+;---Função ExibeMovimento---------
+;Argumento: move
+ExibeMovimento:
+	movf move, W
+	movwf PORTA
+	;emite som (depois)
+	call Delay_500ms ;fake
+	clrf PORTA
+	return
+;---------------------------------
+
+;------Delay 500ms ---------------
+;Temporaria
+Delay_500ms:
+	return
+
+;---------------------------------	
+	
+;------Função ExibeSequencia------
+ExibeSequencia:
+	movlw move_vetor
+	movwf FSR
+	bcf STATUS, IRP
+	clrf exibe_index
+LoopExibe:
+	movf INDF, W
+	movwf move
+	call ExibeMovimento
+	incf FSR, F
+	incf exibe_index, F
+	movf move_index, W
+	subwf exibe_index, W
+	btfss STATUS, Z
+	goto LoopExibe
+	return
+
+;---------------------------------
 
 Delay4cy
 	return
